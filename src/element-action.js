e9742137609d48649dbaa1aab326d961765c65c6
Chintada <cchintada@axcess-financial.com>
2019-05-13 17:37:50 -0400

    browser-action

--- src/element-action.js
+++ src/element-action.js
@@ -4,32 +4,32 @@ module.exports = function(RED) {
 
     function elementAction(config) {
         RED.nodes.createNode(this, config)
-        const node = this
+        const node = this        
         common.clearStatus(node)
 
         node.on('input', async (msg) => {
             try {
                 let browser = await common.getBrowser(node.context())
                 let elementId = await common.getElementId(browser, config.locateUsing, config.locateValue)
+
+                let value = config.sendKeys || msg.value
+                let attribute  = config.attribute || msg.attribute
                 
                 if (config.action === 'click') {
                     await browser.elementClick(elementId)
                 } else if (config.action === 'clear') {
                     await browser.elementClear(elementId)
                 } else if (config.action === 'sendKeys') {
-                    await browser.elementSendKeys(elementId, Array.from(config.sendKeys))
+                    await browser.elementSendKeys(elementId, Array.from(value))
                 }
                 else if (config.action === 'getValue') {
                     msg.payload = await browser.getElementAttribute(elementId,'value')
                 }
                 else if (config.action === 'getText') {
                     msg.payload = await browser.getElementText(elementId)
-                } 
-                /*else if (config.action === 'setValue') {
-                    await browser.addValue(elementId,'value')
-                }*/
+                }
                 else if (config.action === 'getAttribute') {
-                    msg.payload = await browser.getElementAttribute(elementId, config.attribute)
+                    msg.payload = await browser.getElementAttribute(elementId, attribute)
                 }
                 else if(config.action === 'takeScreenShot'){
                     msg.payload = await browser.takeElementScreenshot(elementId)

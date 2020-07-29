d8de268c2da1176068778bc7c06dbb925f82b890
Chintada <cchintada@axcess-financial.com>
2020-07-29 16:12:47 -0400

    using context variables in sendkeys

--- src/element-action.js
+++ src/element-action.js
@@ -4,28 +4,61 @@ module.exports = function(RED) {
   function elementAction(config) {
     RED.nodes.createNode(this, config)
     const node = this
+    const context = node.context()
     common.clearStatus(node)
+
+    var getTypeInputValue = async (msg, type, value) => {
+      var r = ''
+      switch (type) {
+        case 'msg':
+          r = RED.util.getMessageProperty(msg, value)
+          break
+        case 'flow':
+          r = context.flow.get(value)
+          break
+        case 'global':
+          r = context.global.get(value)
+          break
+        case 'str':
+          try {
+            r = unescape(JSON.parse('"' + value + '"'))
+          } catch (e) {
+            r = value
+          }
+          break
+        case 'num':
+          r = parseFloat(value)
+          break
+        case 'json':
+          if (value !== '') {
+            r = JSON.parse(value)
+          } else {
+            r = undefined
+          }
+      }
+      return r
+    }
 
     node.on('input', async (msg) => {
       try {
         let locateUsing = config.locateUsing || msg.locateUsing
         let locateValue = config.locateValue || msg.locateValue
 
-        let browser = await common.getBrowser(node.context())
+        let browser = await common.getBrowser(context)
         let elementId = await common.getElementId(
           browser,
           locateUsing,
           locateValue
         )
 
-        let value = config.sendKeys || msg.value
+        let value = await getTypeInputValue(msg, config.object, config.sendKeys)
         let attribute = config.attribute || msg.attribute
 
         if (config.action === 'click') {
           await browser.elementClick(elementId)
         } else if (config.action === 'clear') {
           await browser.elementClear(elementId)
-        } else if (config.action === 'sendKeys') {
+        } else if (config.action === 'sendKeys' && value) {
           await browser.elementSendKeys(elementId, Array.from(value))
         } else if (config.action === 'getValue') {
           msg.payload = await browser.getElementAttribute(elementId, 'value')
